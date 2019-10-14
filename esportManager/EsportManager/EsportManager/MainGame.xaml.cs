91d787d5d92b71ca477475133b368d395aae353d
st52558 <st52558@student.upce.cz>
2019-10-14 08:09:55 +0200

    again

--- esportManager/EsportManager/EsportManager/MainGame.xaml.cs
+++ esportManager/EsportManager/EsportManager/MainGame.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace EsportManager
 {
@@ -9,15 +13,91 @@ namespace EsportManager
     public partial class MainGame : Window
     {
         public string DatabaseName { get; set; }
+        List<TeamSectionBasic> sectionsList = new List<TeamSectionBasic>();
         public MainGame()
         {
             InitializeComponent();
             
+            AddSectionsToList();
+            SetTabs();
+        }
+
+        private void AddSectionsToList()
+        {
+            // už tady se bude pomocí dotazu zjišťovat jestli jde o B tým (následně se tam to B za to přidá)
+            sectionsList.Add(new TeamSectionBasic(1,2, "League of Legends"));
+            sectionsList.Add(new TeamSectionBasic(3,1, "Counter Strike"));
+            sectionsList.Add(new TeamSectionBasic(6,3, "League of Legends"));
+        }
+
+        private void SetTabs()
+        {
+            int numberOfSections = GetNumberOfSections();
+            while (numberOfSections < SectionTabs.Items.Count)
+            {
+                SectionTabs.Items.RemoveAt(numberOfSections);
+            }
+            switch (numberOfSections)
+            {
+                case 1:
+                    SetTabsDesign(Section1, sectionsList.ElementAt(0));
+                    break;
+                case 2:
+                    SetTabsDesign(Section1, sectionsList.ElementAt(0));
+                    SetTabsDesign(Section2, sectionsList.ElementAt(1));
+                    break;
+                case 3:
+                    SetTabsDesign(Section1, sectionsList.ElementAt(0));
+                    SetTabsDesign(Section2, sectionsList.ElementAt(1));
+                    SetTabsDesign(Section3, sectionsList.ElementAt(2));
+                    break;
+                case 4:
+                    SetTabsDesign(Section1, sectionsList.ElementAt(0));
+                    SetTabsDesign(Section2, sectionsList.ElementAt(1));
+                    SetTabsDesign(Section3, sectionsList.ElementAt(2));
+                    SetTabsDesign(Section4, sectionsList.ElementAt(3));
+                    break;
+                case 5:
+                    SetTabsDesign(Section1, sectionsList.ElementAt(0));
+                    SetTabsDesign(Section2, sectionsList.ElementAt(1));
+                    SetTabsDesign(Section3, sectionsList.ElementAt(2));
+                    SetTabsDesign(Section4, sectionsList.ElementAt(3));
+                    SetTabsDesign(Section5, sectionsList.ElementAt(4));
+                    break;
+                case 6:
+                    SetTabsDesign(Section1, sectionsList.ElementAt(0));
+                    SetTabsDesign(Section2, sectionsList.ElementAt(1));
+                    SetTabsDesign(Section3, sectionsList.ElementAt(2));
+                    SetTabsDesign(Section4, sectionsList.ElementAt(3));
+                    SetTabsDesign(Section5, sectionsList.ElementAt(4));
+                    SetTabsDesign(Section6, sectionsList.ElementAt(5));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void SetTabsDesign(TabItem tab, TeamSectionBasic teamSectionBasic)
+        {
+            Brush brush = Brushes.Black;
+            
+            if (teamSectionBasic.sectionID % 4 == 0) { brush = Brushes.Brown; }
+            if (teamSectionBasic.sectionID % 4 == 1) { brush = Brushes.Cyan; }
+            if (teamSectionBasic.sectionID % 4 == 2) { brush = Brushes.Salmon; }
+            if (teamSectionBasic.sectionID % 4 == 3) { brush = Brushes.Snow; }
+
+            tab.Background = brush;
         }
 
         private void GoToNextDay(object sender, RoutedEventArgs e)
         {
-           
+            
+        }
+
+        private int GetNumberOfSections()
+        {
+            return sectionsList.Count;
         }
 
         private void ShowRegisteredTournaments(object sender, RoutedEventArgs e)
---
+++ esportManager/EsportManager/EsportManager/TeamSectionBasic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsportManager
+{
+    class TeamSectionBasic
+    {
+        public int ID { get; set; }
+        public int sectionID { get; set; }
+        public string sectionName { get; set; }
+
+        public TeamSectionBasic(int iD, int sectionID, string sectionName)
+        {
+            ID = iD;
+            this.sectionID = sectionID;
+            this.sectionName = sectionName;
+        }
+    }
+}
